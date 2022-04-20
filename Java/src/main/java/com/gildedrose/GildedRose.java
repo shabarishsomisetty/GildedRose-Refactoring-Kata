8c41b502d46a1d228600d3ac517c3b77a7c2c856
shabarishsomisetty <somisetti.shabari@gmail.com>
2022-04-20 19:00:07 +0530

    Changed constant names to capital as per Java standards Created new methods for increase and decrease quality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,9 +1,9 @@
 package com.gildedrose;
 
 class GildedRose {
-    private final String agedBrie = "Aged Brie";
-    private final String sulfuras = "Sulfuras, Hand of Ragnaros";
-    private final String backstageConcert = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String BACKSTAGE_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
 
     Item[] items;
 
@@ -13,54 +13,58 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item: items) {
-            if (!agedBrie.equals(item.name)
-                    && !backstageConcert.equals(item.name)) {
+            if (!AGED_BRIE.equals(item.name)
+                    && !BACKSTAGE_CONCERT.equals(item.name)) {
                 if (item.quality > 0) {
-                    if (!item.name.equals(sulfuras)) {
-                        item.quality = item.quality - 1;
+                    if (!SULFURAS.equals(item.name)) {
+                        decreaseQuality(item);
                     }
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    increaseQuality(item);
 
-                    if (backstageConcert.equals(item.name)) {
+                    if (BACKSTAGE_CONCERT.equals(item.name)) {
                         if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                            increaseQuality(item);
                         }
 
                         if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                            increaseQuality(item);
                         }
                     }
                 }
             }
 
-            if (!sulfuras.equals(item.name)) {
+            if (!SULFURAS.equals(item.name)) {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!agedBrie.equals(item.name)) {
-                    if (!backstageConcert.equals(item.name)) {
+                if (!AGED_BRIE.equals(item.name)) {
+                    if (!BACKSTAGE_CONCERT.equals(item.name)) {
                         if (item.quality > 0) {
-                            if (!item.name.equals(sulfuras)) {
-                                item.quality = item.quality - 1;
+                            if (!item.name.equals(SULFURAS)) {
+                                decreaseQuality(item);
                             }
                         }
                     } else {
-                        item.quality = item.quality - item.quality;
+                        item.quality = 0;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    increaseQuality(item);
                 }
             }
         }
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        item.quality = item.quality - 1;
     }
 }
